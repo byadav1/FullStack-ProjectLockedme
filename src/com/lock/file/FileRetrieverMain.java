9b60fa0e5519599f033ce940237ba7e66e5205f2
yadav <yadavbg@gmail.com>
2022-10-09 20:25:49 -0400

    adding prject folder

---
+++ src/com/lock/file/FileRetrieverMain.java
@@ -0,0 +1,10 @@
+package com.lock.file;
+
+public class FileRetrieverMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yadav
+ *
+ */
+module LockedMe {
+}
