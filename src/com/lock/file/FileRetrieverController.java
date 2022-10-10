7aeccce3c556295fb08fbae0ef29d3ac80891306
yadav <yadavbg@gmail.com>
2022-10-09 22:50:10 -0400

    Added the logic to sort the file name ascending  order in the current directory

--- src/com/lock/file/FileRetrieverController.java
+++ src/com/lock/file/FileRetrieverController.java
@@ -3,50 +3,56 @@ package com.lock.file;
 import java.util.Scanner;
 
 public class FileRetrieverController {
-	
-	private Scanner console ;
-	private String defaultDirectory= "C:\\Users";
+
+	private Scanner console;
+	private String defaultDirectory = "C:\\Users";
 	private FileRetrieverModel retreiver;
-	public FileRetrieverController (){
+
+	public FileRetrieverController() {
 		this.retreiver = new FileRetrieverModel();
-		this.console= new Scanner(System.in);
-		
+		this.console = new Scanner(System.in);
+
 	}
-	
+
 	public void displayUserOptions() {
+
+		int userSelection;
+		boolean validInput = false;
+		do {
+			System.out.print("Please enter 1 to display current directory details \n");
+			System.out.print("Please enter 2 to perform add/delete/search operation on directory \n");
+			System.out.print("Please enter 3 to exit from the application \n");
+			userSelection = this.console.nextInt();
+			switch (userSelection) {
+			case 1:
+				System.out.println("Directory name- " + System.getProperty("user.dir") + " and details are as follow");
+				this.retreiver.currentDirectoryDetails();
+				validInput = true;
+				break;
+			case 2:
+				System.out.println("Choose to perform add/delete/search operation on directory");
+				break;
+			case 3:
+				System.out.println("GoodBye!! Thank you for using this application");
+				validInput = false;
+				break;
+			default:
+				System.out.println("Invalid input");
+				validInput = true;
+				break;
+			}
+		} while (validInput);
 		
-		int userSelection; 
-			
-		System.out.print("Please enter 1 to display current directory details \n");
-		System.out.print("Please enter 2 to perform add/delete/search operation on directory \n");
-		userSelection = this.console.nextInt();		
-		switch (userSelection) {
-		case 1:
-			System.out.println("Directory name- " +System.getProperty("user.dir") + " and details are as follow");
-			this.retreiver.currentDirectoryDetails();
-			break;
-
-		case 2:
-			System.out.println("Delete a user specified file from the existing directory list.");
-			break;
-
-		case 3:
-			System.out.println("Search a user specified file from the main directory");
-			break;
-
-		default:
-			System.out.println("Invalid input");
-		}
 	}
 
 	private void displayFileOpertaionUserOptions() {
-		
-		int userSelection; 
-			
+
+		int userSelection;
+
 		System.out.print("Please enter 1 to add a file to the existing directory list \n");
 		System.out.print("Please enter 2 to delete a user specified file from the existing directory list.\n");
 		System.out.print("Please enter 3 to Search a user specified file from the main directory.\n ");
-		userSelection = this.console.nextInt();		
+		userSelection = this.console.nextInt();
 		switch (userSelection) {
 		case 1:
 			System.out.println("Add a file to the existing directory list");
--- src/com/lock/file/FileRetrieverModel.java
+++ src/com/lock/file/FileRetrieverModel.java
@@ -1,23 +1,23 @@
 package com.lock.file;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class FileRetrieverModel {
-	
-	
-	
+
 	public void currentDirectoryDetails() {
 		String dir = System.getProperty("user.dir");
-		File curDir = new File(dir);
-		
-		 File[] filesList = curDir.listFiles();
-	        for(File f : filesList){
-	            if(f.isDirectory())
-	                System.out.println(f.getName());
-	            if(f.isFile()){
-	                System.out.println(f.getName());
-	            }
-	        }
+		File curDir = new File("C:\\Users");
+		// File[] filesList = curDir.listFiles();
+		List<String> listfileName = Arrays.asList(curDir.list());
+		Collections.sort(listfileName, String.CASE_INSENSITIVE_ORDER);
+
+		for (String f : listfileName) {
+			System.out.println(f);
+		}
+		System.out.println("----------------------------------------------------");
 	}
 
 }
